1be900495053aa17eb234102efb0f1c6c4b7346f
marcopriests <prtmrc98@gmail.com>
2025-03-25 15:40:15 +0100

    [BONUS] bonus completato

--- js/script.js
+++ js/script.js
@@ -4,11 +4,18 @@ let bulb = document.getElementById('bulb')
 let button = document.querySelector('.btn')
 
 //definisco il comportamento del bottone
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
 
-    
-    body.style.backgroundColor = "white"
-    button.innerHTML = 'Spegni la luce'
-    bulb.src = '../img/yellow_lamp.png'
+
+    //effettuo il controllo per rendere ripetibile l'accensione
+    if (body.style.backgroundColor == "black") {
+        body.style.backgroundColor = "white"
+        button.innerHTML = 'Spegni la luce'
+        bulb.src = '../img/yellow_lamp.png'
+    } else {
+        body.style.backgroundColor = "black"
+        button.innerHTML = 'Accendi la luce'
+        bulb.src = '../img/white_lamp.png'
+    }
 
 })
